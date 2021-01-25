4d7e34a6cc44b7969ee6154b289901f862a9e7bd
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 10:47:28 +0100

    Add Visits for given user_id

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -26,4 +25,26 @@ class VisitController extends Controller
             return response()  -> json(['success' => $visit], 201);
         }
     }
+    public function index_for_user(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required'
+        ]);
+        if($validator->fails())
+            return response()->json(['error' => $validator->errors()], 405);
+        else 
+        {
+            $visits = \App\Models\Visit::where('user_id', '=', $request->all()['user_id'])
+                -> orWhere('doctor_id', '=', $request->all()['user_id'])->get(['date', 'doctor_id']);
+            foreach($visits as $v)
+            {
+                $name = \App\Models\User::find($v['doctor_id'])->name;
+                $specialization = \App\Models\Doctor::where('user_id', $v['doctor_id'])->get('specialization');
+                $v['doctor_name'] = $name;
+                $v['specialization'] = $specialization[0]->specialization;
+            }
+            return response()->json($visits, 200);
+        }
+    }
 }
+?>
